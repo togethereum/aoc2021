5e3f70b692f8e9cef1bcbf75ecd00353f272a24e
Adam Schmideg <adam@schmideg.net>
2021-12-06 21:52:59 +0100

    Draw line on board

--- clojure/src/aoc2021/day05.clj
+++ clojure/src/aoc2021/day05.clj
@@ -12,7 +12,29 @@
 
 (defn init-matrix
   [x y]
-  (repeat x (repeat y 0)))
+  (vec
+    (repeat x
+          (vec (repeat y 0)))))
+
+(defn points-of-line
+  [[x1 y1 x2 y2]]
+  (cond
+   (= x1 x2)
+   (for [i (range (min y1 y2) (inc (max y1 y2)))]
+     [x1 i])
+   (= y1 y2)
+   (for [i (range (min x1 x2) (inc (max x1 x2)))]
+     [i y1])
+   :else
+   nil))
+
+(defn draw-line
+  [board line]
+  (let [points (points-of-line line)]
+    (reduce (fn [board point]
+              (update-in board point inc))
+            board
+            points)))
 
 (defn solve-1
   [lines])
--- clojure/test/aoc2021/day05_test.clj
+++ clojure/test/aoc2021/day05_test.clj
@@ -13,3 +13,20 @@
 (deftest init-matrix-test
   (is (= (init-matrix 3 4)
          [[0 0 0 0] [0 0 0 0] [0 0 0 0]])))
+
+(deftest points-of-line-test
+  (is (= (points-of-line [1 2 3 2])
+         [[1 2] [2 2] [3 2]]))
+  (is (= (points-of-line [3 2 1 2])
+         [[1 2] [2 2] [3 2]]))
+  (is (= (points-of-line [1 2 1 4])
+         [[1 2] [1 3] [1 4]]))
+  (is (= (points-of-line [1 4 1 2])
+         [[1 2] [1 3] [1 4]])))
+
+(deftest draw-line-test
+  (let [m (init-matrix 3 4)]
+    (is (= (draw-line m [0 1, 1 1])
+           [[0 1 0 0]
+            [0 1 0 0]
+            [0 0 0 0]]))))
