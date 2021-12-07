c4d757efa358ea71b6d5c28a301e01ab6f570e35
Adam Schmideg <adam@schmideg.net>
2021-12-07 22:07:29 +0100

    Day 6 puzzle 1

--- clojure/test/aoc2021/day06_test.clj
+++ clojure/test/aoc2021/day06_test.clj
@@ -1,2 +1,18 @@
 (ns aoc2021.day06-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [aoc2021.day06 :refer :all]))
+
+(deftest next-day-test
+  (is (= (next-day [0 2 0])
+         [6 1 6 8 8])))
+
+(def test-input [3,4,3,1,2])
+
+(deftest count-after-n-days-test
+  (is (= (count-after-n-days 18 test-input)
+        26))
+  (is (= (count-after-n-days 80 test-input)
+        5934)))
+
+(deftest solve-1-test
+  (println (solve-1)))
