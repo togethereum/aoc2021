5f98dedeb386a3e1cf0fcd9cc62cb53f88caa1fc
Adam Schmideg <adam@schmideg.net>
2022-01-27 21:00:40 +0100

    Elixir skeleton

---
+++ elixir/lib/puzzle01.ex
@@ -0,0 +1,11 @@
+defmodule Puzzle01 do
+  @moduledoc """
+  See [Simple array sum | Hackerrank](https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true)
+  """
+
+  @doc """
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir/test/puzzle01_test.exs
@@ -0,0 +1,7 @@
+defmodule Puzzle01Test do
+  use ExUnit.Case
+
+  test "greets the world" do
+    assert Puzzle01.hello() == :world
+  end
+end
