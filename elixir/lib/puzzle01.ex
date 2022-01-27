ae70d222606eab3844daaaa5a65c2f20ada48fce
Adam Schmideg <adam@schmideg.net>
2022-01-27 22:40:03 +0100

    Staircase problem solved

--- elixir/lib/puzzle01.ex
+++ elixir/lib/puzzle01.ex
@@ -1,11 +1,18 @@
 defmodule Puzzle01 do
   @moduledoc """
-  See [Simple array sum | Hackerrank](https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true)
+  See [Staircase | Hackerrank](https://www.hackerrank.com/challenges/staircase/problem?isFullScreen=true)
   """
 
-  @doc """
-  """
-  def hello do
-    :world
+  def lines(1) do ["#"] end
+
+  def lines(n) when n > 1 do
+    upper = for line <- lines(n - 1), do: " " <> line
+    upper ++ [String.duplicate("#", n)]
   end
+
+  def solve(n) when n > 0 do
+    Enum.join(lines(n), "\n")
+  end
+
+  def solve(n) do "" end
 end
--- elixir/test/puzzle01_test.exs
+++ elixir/test/puzzle01_test.exs
@@ -1,7 +1,20 @@
 defmodule Puzzle01Test do
   use ExUnit.Case
 
-  test "greets the world" do
-    assert Puzzle01.hello() == :world
+  test "Lines" do
+    assert Puzzle01.lines(2) == [" #", "##"]
+  end
+
+  test "Smallest staircase" do
+    assert Puzzle01.solve(1) == "#"
+  end
+
+  test "Larger staircases" do
+    assert Puzzle01.solve(2) == " #\n##"
+    assert Puzzle01.solve(3) == "  #\n ##\n###"
+  end
+
+  test "Unnatural staircase" do
+    assert Puzzle01.solve(-1) == ""
   end
 end
